7cf464398ddfab4d1a08d57ecf5cb7adfc40e6ad
Vitalii Zakharov <lewiss945@gmail.com>
2016-09-08 10:50:35 +0300

    Amazon products displayed

--- src/AmazonProducts/Controllers/HomeController.cs
+++ src/AmazonProducts/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AmazonProductAdvertisingAPI;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace AmazonProducts.Controllers
 {
@@ -23,6 +26,49 @@ namespace AmazonProducts.Controllers
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Amazon()
+        {
+            string associateTag = "***";
+            string awsAccessKeyId = "***";
+            string awsSecretKey = "***";
+
+            var keywords = "wcf";
+            string responseJson = string.Empty;
+
+            if (string.IsNullOrEmpty(keywords))
+            {
+                responseJson = AmazonApiHelper.GetEmptyResponseJson(string.Empty, string.Empty, HttpStatusCode.OK);
+            }
+            else
+            {
+                using (AmazonApiHelper apiHelper = new AmazonApiHelper(associateTag, awsAccessKeyId, awsSecretKey))
+                {
+                    string requestUri = apiHelper.GetRequestUri(keywords);
+                    try
+                    {
+                        responseJson = apiHelper.ExecuteWebRequest(requestUri, keywords);
+                    }
+                    catch (Exception ex)
+                    {
+                        responseJson = AmazonApiHelper.GetEmptyResponseJson(keywords, ex.Message, HttpStatusCode.InternalServerError);
+                    }
+                }
+            }
+
+            dynamic stuff = JsonConvert.DeserializeObject(responseJson);
+
+            var products = new List<dynamic>();
+            foreach (var item in stuff.responseArray)
+            {
+                products.Add(item);
+            }
+
+            ViewBag.Keywords = stuff.keywords;
+            ViewBag.Products = products;
 
             return View();
         }
